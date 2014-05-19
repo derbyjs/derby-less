c10f8cbace3d7f55f7719a2acf8ce8d635ee342e
Artur Zayats <zag2art@gmail.com>
2014-05-19 09:31:00 +0400

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 derby-less
 ==========
 
-Derby plugin to add Less support
+# Derby plugin to add Less support
+
+## Usage
+
+Right after creating your Derby app, add:
+
+```js
+app.serverUse(module, 'derby-less');
+```
+
+Make sure this is before any calls to `app.loadViews()`.
+
+After that you can use `*.less` files instead of `*.css`
---
+++ index.js
@@ -0,0 +1,22 @@
+module.exports = function(app) {
+  app.viewExtensions.push('.less');
+  app.compilers['.less'] = lessCompiler;
+};
+
+function lessCompiler(file, filename, options) {
+  var parser = new less.Parser({
+    paths: [path.dirname(filename)]
+    , filename: filename
+    , syncImport: true
+  });
+  var out = {};
+  parser.parse(file, function(err, tree) {
+    if (err) throw err;
+    out.css = tree.toCSS(options);
+  });
+  out.files = Object.keys(parser.imports.files).map(function(path) {
+    return path;
+  });
+  out.files.push(filename);
+  return out;
+}
